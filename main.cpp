b0cbe7c304e21dafa43e688143c795124fdfe562
NotAkiDev <tktelnykh@edu.hse.ru>
2023-10-30 11:47:05 +0300

    tween_factorial function created

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std;
+
+
+long tween_factorial(long stop) {
+    long summator = 1;
+    int i;
+    if (not(stop % 2)) {
+        i = 2;
+    } else {
+        i = 1;
+    }
+    for (; i <= stop; i += 2) {
+        summator *= i;
+    }
+
+    return summator;
+}
+
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
