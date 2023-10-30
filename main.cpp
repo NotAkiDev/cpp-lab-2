75e47217f7a1a76ad7e5fa47fcfe25d261952197
NotAkiDev <tktelnykh@edu.hse.ru>
2023-10-30 11:52:13 +0300

    base of t_function, pow and total_answer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+double pow(double num, int power) {
+    double to_return = 1;
+    for (int i = 0; i < power; i++)
+        to_return *= num;
+    return to_return;
+}
 
 long tween_factorial(long stop) {
     long summator = 1;
@@ -17,9 +23,16 @@ long tween_factorial(long stop) {
     return summator;
 }
 
+double t(double x) {}
 
+double total_answer(double y) {
+    return (7 * t(0.25) + 2 * t(1 + y)) / (6 - t(pow(y, 2) - 1));
+}
 
 
 int main() {
+    double num = 0;
+    cin >> num;
+    cout << total_answer(num);
     return 0;
 }
